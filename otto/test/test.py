f206424c3e5d6f43080dbae8c68aa2186e8ff349
aurore.loisy <aurore.loisy@gmail.com>
2022-02-16 17:33:05 +0100

    edit readthedocs.yaml

---
+++ otto/test/test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Script used to test the docs
+
+The list of all parameters is given below.
+"""
+
+def run():
+    print("this is a test")
+
+if __name__ == "__main__":
+    run()
+
+    
+
